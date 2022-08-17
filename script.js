444df85d17b4aed1f7b1e982a91f0191134deda5
yahya.codin <yahya.codin@gmail.com>
2022-08-17 17:30:21 +0300

    m

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-AOS.init();
+AOS.init({ disable: "mobile" });
+
+$(".navbar-toggler").click(function () {
+  $("#navmenu").toggle();
+});
+$("#navmenu").click(function () {
+  $("#navmenu").hide();
+});
+
+function clearFields() {
+  document.querySelector("#input").value = "";
+  document.querySelector("#textarea").value = "";
+}
+
+document.querySelector("#send").addEventListener("click", function () {
+  clearFields();
+});
